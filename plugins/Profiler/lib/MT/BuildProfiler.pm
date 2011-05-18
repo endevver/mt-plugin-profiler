50dae26cfb15e98a22f030d272ca43e28a2b6be8
Jay Allen <jay@endevver.com>
2011-05-17 19:09:51 -0700

    Perltidy and minor edits

--- plugins/Profiler/lib/MT/BuildProfiler.pm
+++ plugins/Profiler/lib/MT/BuildProfiler.pm
@@ -1,48 +1,57 @@
 package MT::BuildProfiler;
 
-use strict;
+# TODO Turn this utility library into a real object-oriented class
 
+use strict;
+use warnings;
 use MT::Util qw( epoch2ts );
 use Time::HiRes;
 use Data::Dumper;
 
-my %PROFILES;
-my $BUILD_START_AT;
-my $BUILD_ELAPSED;
-our ( %EXDATA );
+# TODO Get rid of package variables in favor of object properties
+our ( %PROFILES, $BUILD_START_AT, $BUILD_ELAPSED, %EXDATA );
 
 sub report {
     Data::ObjectDriver->profiler->reset;
-    my $driver = MT::Object->driver;
-    my $total_time = 0;
+    my $driver      = MT::Object->driver;
+    my $total_time  = 0;
     my $total_query = 0;
+    my @tags = sort { $PROFILES{$b}{time} <=> $PROFILES{$a}{time} }
+                      keys %PROFILES;
+
     my @rows;
-    foreach my $tag ( sort { $PROFILES{$b}{time} <=> $PROFILES{$a}{time} } keys %PROFILES ) {
-#        my $queries = $PROFILES{$tag}{queries} || [];
-        my $time_avg = sprintf("%0.3f", $PROFILES{$tag}{time} / $PROFILES{$tag}{calls});
-        my $queries = $PROFILES{$tag}->{queries};
-        my $ramhit = map { $_ =~ m/RAMCACHE_GET/ } @$queries;
-        my $ramadd = map { $_ =~ m/RAMCACHE_ADD/ } @$queries;
-        my $query_count = scalar(@$queries) - ($ramhit + $ramadd);
-        push @rows, { 
-	    time => sprintf("%0.3f", $PROFILES{$tag}{time}),
-	    tag => $tag, 
-	    calls => $PROFILES{$tag}{calls},
-            avg => $time_avg,
+    foreach my $tag ( @tags ) {
+
+        # my $queries = $PROFILES{$tag}{queries} || [];
+        my $time_avg = sprintf( "%0.3f",
+            $PROFILES{$tag}{time} / $PROFILES{$tag}{calls} );
+        my $queries     = $PROFILES{$tag}->{queries};
+        my $ramhit      = map { $_ =~ m/RAMCACHE_GET/ } @$queries;
+        my $ramadd      = map { $_ =~ m/RAMCACHE_ADD/ } @$queries;
+        my $query_count = scalar(@$queries) - ( $ramhit + $ramadd );
+        push @rows,
+          {
+            time    => sprintf( "%0.3f", $PROFILES{$tag}{time} ),
+            tag     => $tag,
+            calls   => $PROFILES{$tag}{calls},
+            avg     => $time_avg,
             queries => $query_count,
-            hits => $ramhit, 
-	    misses => $ramadd,
-	};
-        $total_time += $PROFILES{$tag}{time};
+            hits    => $ramhit,
+            misses  => $ramadd,
+          };
+        $total_time  += $PROFILES{$tag}{time};
         $total_query += $query_count;
+
         # Restore D::OD query profile so the report we will generate
         # from it will be right:
-        foreach my $q (@$queries) {
-            Data::ObjectDriver->profiler->record_query($driver, $q);
-        }
-    }
+        Data::ObjectDriver->profiler->record_query( $driver, $_ )
+            foreach @$queries;
+
+    } ## end foreach my $tag ( sort { $PROFILES...})
+
     return @rows;
-}
+
+} ## end sub report
 
 sub install {
     my $all_tags = MT::Component->registry("tags");
@@ -50,7 +59,7 @@ sub install {
         for my $type (qw( block function )) {
             my $tags = $tag_set->{$type} or next;
             for my $tagname ( keys %$tags ) {
-                $tags->{$tagname} = _make_tracker($tags->{$tagname});
+                $tags->{$tagname} = _make_tracker( $tags->{$tagname} );
             }
         }
     }
@@ -59,12 +68,12 @@ sub install {
 sub _make_tracker {
     my $original_method = shift;
     return sub {
-        my ($ctx, $args, $cond) = @_;
+        my ( $ctx, $args, $cond ) = @_;
         my $tagname = lc $ctx->stash('tag');
-        pre_process_tag($ctx, $args, $cond);
+        pre_process_tag( $ctx, $args, $cond );
         my $meth = MT->handler_to_coderef($original_method)
-            unless $original_method eq 'CODEREF';
-        my $res = $meth->($ctx, $args, $cond);
+          unless $original_method eq 'CODEREF';
+        my $res = $meth->( $ctx, $args, $cond );
         post_process_tag($res);
         return $res;
     };
@@ -75,9 +84,9 @@ sub _make_tracker {
     my @PROFILE_STACK;
 
     sub pre_process_tag {
-        my ($ctx, $args, $cond) = @_;
+        my ( $ctx, $args, $cond ) = @_;
         $BUILD_START_AT = [ Time::HiRes::gettimeofday() ]
-            unless defined $BUILD_START_AT;
+          unless defined $BUILD_START_AT;
         if ( defined $CURRENT_TAG ) {
             $CURRENT_TAG->pause;
             push @PROFILE_STACK, $CURRENT_TAG;
@@ -89,24 +98,25 @@ sub _make_tracker {
     sub post_process_tag {
         my ($res) = @_;
         $CURRENT_TAG->end;
+
         #save results to hash
         my $results = $PROFILES{ $CURRENT_TAG->{tagname} };
         if ( defined $results ) {
             $results->{calls} += 1;
-            $results->{time} += $CURRENT_TAG->{time};
-            push @{$results->{queries}}, @{$CURRENT_TAG->{queries}};
+            $results->{time}  += $CURRENT_TAG->{time};
+            push @{ $results->{queries} }, @{ $CURRENT_TAG->{queries} };
         }
         else {
             $PROFILES{ $CURRENT_TAG->{tagname} } = {
-                calls => 1,
-                time  => $CURRENT_TAG->{time},
-                queries => $CURRENT_TAG->{queries},
+                                           calls   => 1,
+                                           time    => $CURRENT_TAG->{time},
+                                           queries => $CURRENT_TAG->{queries},
             };
         }
         $CURRENT_TAG = pop @PROFILE_STACK;
         $CURRENT_TAG->resume if defined $CURRENT_TAG;
         $BUILD_ELAPSED = Time::HiRes::tv_interval($BUILD_START_AT);
-    }
+    } ## end sub post_process_tag
 }
 
 package TagProfiler;
@@ -116,25 +126,24 @@ use Time::HiRes;
 use Data::ObjectDriver;
 
 sub new {
-    my $class = shift;
-    my ($tagname) = @_;
-    my $now = [ Time::HiRes::gettimeofday() ];
+    my $class       = shift;
+    my ( $tagname ) = @_;
+    my $now         = [ Time::HiRes::gettimeofday() ];
     Data::ObjectDriver->profiler->reset;
-    return bless {
-        tagname => $tagname,
-        last    => $now,
-        time    => 0,
-        queries => [],
-     }, $class;
+    return bless {  tagname => $tagname,
+                    last => $now,
+                    time => 0,
+                    queries => []
+            }, $class;
 }
 
 sub pause {
     my $self = shift;
-    $self->{time} += Time::HiRes::tv_interval($self->{last});
+    $self->{time} += Time::HiRes::tv_interval( $self->{last} );
     my $log = Data::ObjectDriver->profiler->query_log;
-    if ( defined $log && scalar @$log ) {
-        @{$self->{queries}} = (@{$self->{queries}}, @$log );
-    }
+
+    $self->{queries} = [ @{ $self->{queries} }, @$log ]
+        if ref $log eq 'ARRAY' and @$log;
 }
 
 sub resume {
--- plugins/Profiler/lib/Profiler/Plugin.pm
+++ plugins/Profiler/lib/Profiler/Plugin.pm
@@ -7,16 +7,21 @@ use MT::BuildProfiler;
 
 sub itemset_profile_template {
     my $app = shift;
-    my $q = $app->{query};
+    my $q   = $app->{query};
     $app->validate_magic or return $app->error("Invalid magic");
     my @tmpls = $q->param('id');
+
     # TODO - this should only ever be run against a single template
     # Perhaps error if more than one template profiled?
     for my $tmpl_id (@tmpls) {
-        my $tmpl = MT->model('template')->load($tmpl_id) 
-	    or return $app->error( "Unable to load template #" . $tmpl_id );
-	MT->log({ blog_id => $app->blog->id, message => "Profiling template: " . $tmpl->name });
-	return results($app, $tmpl);
+        my $tmpl = MT->model('template')->load($tmpl_id)
+          or return $app->error( "Unable to load template #" . $tmpl_id );
+        MT->log( {
+                   blog_id => $app->blog->id,
+                   message => "Profiling template: " . $tmpl->name
+                 }
+        );
+        return results( $app, $tmpl );
     }
 }
 
@@ -25,16 +30,16 @@ sub profile_build {
 }
 
 sub results {
-    my $app = shift;
+    my $app    = shift;
     my ($tmpl) = @_;
-    my $blog = $app->blog;
+    my $blog   = $app->blog;
 
     require MT::Template::Context;
     require MT::Template::ContextHandlers;
     require MT::Builder;
     require MT::Util;
 
-    $ENV{DOD_PROFILE} = 1;
+    $ENV{DOD_PROFILE}            = 1;
     $Data::ObjectDriver::PROFILE = 1;
     MT::BuildProfiler->install;
 
@@ -42,98 +47,108 @@ sub results {
     $d->{RaiseError} = 1;
 
     my $template = $tmpl->text;
-    my $ctx = MT::Template::Context->new;
+    my $ctx      = MT::Template::Context->new;
 
     my $builder = MT::Builder->new;
-    $template = $app->translate_templatized($template);
+    $template = $app->translate_templatized( $template );
 
-    my $tokens = $builder->compile($ctx, $template);
+    my $tokens = $builder->compile( $ctx, $template );
 
-    $ctx->stash('blog', $app->blog);
-    $ctx->stash('blog_id', $app->blog->id);
+    $ctx->stash( 'blog',    $app->blog );
+    $ctx->stash( 'blog_id', $app->blog->id );
 
     my $archive_type;
+
     # determine archive type for this template
     if ( $tmpl->type eq 'category' ) {
-	$archive_type = 'Category';
+        $archive_type = 'Category';
     }
     elsif ( $tmpl->type eq 'individual' ) {
-	$archive_type = 'Individual';
+        $archive_type = 'Individual';
     }
     elsif ( $tmpl->type eq 'page' ) {
-	$archive_type = 'Page';
+        $archive_type = 'Page';
     }
     elsif ( $tmpl->type eq 'archive' ) {
-	my $map = MT->model('templatemap')->load( { template_id => $tmpl->id, is_preferred => 1 });
-	$archive_type = $map->archive_type if $map;
+        my $map = MT->model('templatemap')
+          ->load( { template_id => $tmpl->id, is_preferred => 1 } );
+        $archive_type = $map->archive_type if $map;
     }
 
 
     my $e;
-    if (defined $archive_type) {
-	MT->log({ blog_id => $app->blog->id, message => 'Initializing: ' . $archive_type });
-	my $t = $app->publisher->archiver($archive_type)
-	    or return $app->error("Invalid archive type '$archive_type'");
+    if ( defined $archive_type ) {
+        MT->log( {
+                   blog_id => $app->blog->id,
+                   message => 'Initializing: ' . $archive_type
+                 }
+        );
+        my $t = $app->publisher->archiver($archive_type)
+          or return $app->error("Invalid archive type '$archive_type'");
 
-	$ctx->{current_archive_type} = $archive_type;
-	$ctx->{archive_type} = $archive_type;
+        $ctx->{current_archive_type} = $archive_type;
+        $ctx->{archive_type}         = $archive_type;
 
-	$e = load_entry( $app, $t->entry_class );
+        $e = load_entry( $app, $t->entry_class );
 
-	require MT::Promise;
-	if ($t->date_based) {
-	    my ($ts_start, $ts_end) = $t->date_range($e->authored_on);
-	    $ctx->{current_timestamp} = $ts_start;
-	    $ctx->{current_timestamp_end} = $ts_end;
-	    my $entries = sub { $t->dated_group_entries($ctx, $archive_type, $ts_start) };
-	    $ctx->stash('entries', MT::Promise::delay($entries));
-	}
-	if ($t->author_based) {
-	    my $a = $e->author || load_author($app);
-	    $ctx->stash('author') = $a;
-	    my $entries = sub { $t->archive_group_entries($ctx) };
-	    $ctx->stash('entries', MT::Promise::delay($entries));
-	}
-	if ($t->category_based) {
-	    my $cat = load_category($app);
-	    $ctx->stash('archive_category', $cat);
-	    my $entries = sub { $t->archive_group_entries($ctx) };
-	    $ctx->stash('entries', MT::Promise::delay($entries));
-	}
-	if ($t->entry_based) {
-	    $ctx->stash('entry', $e);
-	}
-    }
+        require MT::Promise;
+        if ( $t->date_based ) {
+            my ( $ts_start, $ts_end ) = $t->date_range( $e->authored_on );
+            $ctx->{current_timestamp}     = $ts_start;
+            $ctx->{current_timestamp_end} = $ts_end;
+            my $entries = sub {
+                $t->dated_group_entries( $ctx, $archive_type, $ts_start );
+            };
+            $ctx->stash( 'entries', MT::Promise::delay($entries) );
+        }
+        if ( $t->author_based ) {
+            my $a = $e->author || load_author($app);
+            $ctx->stash('author') = $a;
+            my $entries = sub { $t->archive_group_entries($ctx) };
+            $ctx->stash( 'entries', MT::Promise::delay($entries) );
+        }
+        if ( $t->category_based ) {
+            my $cat = load_category($app);
+            $ctx->stash( 'archive_category', $cat );
+            my $entries = sub { $t->archive_group_entries($ctx) };
+            $ctx->stash( 'entries', MT::Promise::delay($entries) );
+        }
+        if ( $t->entry_based ) {
+            $ctx->stash( 'entry', $e );
+        }
+    } ## end if ( defined $archive_type)
     else {
-        if (defined $archive_type) {
+        if ( defined $archive_type ) {
             return $app->error("Cannot specify archive type without a blog.");
         }
     }
 
-    if ($ENV{DOD_PROFILE}) {
+    if ( $ENV{DOD_PROFILE} ) {
         Data::ObjectDriver->profiler->reset;
     }
 
-    my $start = [ gettimeofday ];
-    my $out = $builder->build($ctx, $tokens, {});
-    my $end = [ gettimeofday ];
+    my $start = [gettimeofday];
+    my $out   = $builder->build( $ctx, $tokens, {} );
+    my $end   = [gettimeofday];
 
     my $param ||= {};
-    return $app->error("Builder error: ".$builder->errstr) if $builder->errstr;
-    return $app->error("Context error: ".$ctx->errstr) if $ctx->errstr;
+    return $app->error( "Builder error: " . $builder->errstr )
+      if $builder->errstr;
+    return $app->error( "Context error: " . $ctx->errstr ) if $ctx->errstr;
     my @rows = MT::BuildProfiler->report();
 
-    $param->{rows} = \@rows;
+    $param->{rows}          = \@rows;
     $param->{template_name} = $tmpl->name;
-    $param->{total_time} = tv_interval($start, $end);
-    $param->{return_args} = '__mode=list&_type=template';
+    $param->{total_time}    = tv_interval( $start, $end );
+    $param->{return_args}   = '__mode=list&_type=template';
     return $app->load_tmpl( 'report.tmpl', $param );
-}
+} ## end sub results
 
 sub load_category {
     my $app = shift;
 
     my $cat;
+
 #    if ($category_name =~ m/^\d+$/) {
 #        $cat = MT->model('category')->load($category_name)
 #            or die "Could not load category # $category_name";
@@ -145,51 +160,66 @@ sub load_category {
 #        }) or die "Could not locate category by name '$category_name'";
 #    }
 #    else {
-        # okay, select first available
-	my $c = '= placement_entry_id';
-        my $p = MT->model('placement')->load({
-            ( $app->blog ? ( blog_id => $app->blog->id ) : () ),
-            is_primary => 1,
-            },
-            { limit => 1,
-              join => MT->model('entry')->join_on( undef, {
-                  class => 'entry',
-                  id => \$c,
-                  status => 2,
-              } ), }
-        );
-        if ( $p ) {
-            $cat = MT->model('category')->load( $p->category_id );
-        }
+    # okay, select first available
+    my $c = '= placement_entry_id';
+    my $p = MT->model('placement')->load( { (
+                                           $app->blog
+                                           ? ( blog_id => $app->blog->id )
+                                           : ()
+                                         ),
+                                         is_primary => 1,
+                                       },
+                                       {
+                                         limit => 1,
+                                         join  => MT->model('entry')->join_on(
+                                                          undef,
+                                                          {
+                                                            class  => 'entry',
+                                                            id     => \$c,
+                                                            status => 2,
+                                                          }
+                                         ),
+                                       }
+    );
+    if ($p) {
+        $cat = MT->model('category')->load( $p->category_id );
+    }
+
 #    }
     return $cat;
-}
+} ## end sub load_category
 
 sub load_author {
     my $app = shift;
 
     # TODO
     my $author_name = '';
-    if ($author_name =~ m/^\d+$/) {
+    if ( $author_name =~ m/^\d+$/ ) {
         $a = MT->model('author')->load($author_name)
-            or die "Could not load author # $author_name";
+          or die "Could not load author # $author_name";
     }
-    elsif (defined $author_name) {
-        $a = MT->model('author')->load({ name => $author_name })
-            or die "Could not locate author by name '$author_name'";
+    elsif ( defined $author_name ) {
+        $a = MT->model('author')->load( { name => $author_name } )
+          or die "Could not locate author by name '$author_name'";
     }
     return $a;
 }
 
 sub load_entry {
-    my ($app,$entry_class) = @_;
+    my ( $app, $entry_class ) = @_;
 
     # load first available
     my $e = MT->model('entry')->load( {
-	class => $entry_class,
-	status => 2,
-	blog_id => $app->blog->id,
-        }, { limit => 1, sort => 'authored_on', direction => 'descend' });
+                                        class   => $entry_class,
+                                        status  => 2,
+                                        blog_id => $app->blog->id,
+                                      },
+                                      {
+                                        limit     => 1,
+                                        sort      => 'authored_on',
+                                        direction => 'descend'
+                                      }
+    );
     return $e;
 }
 
